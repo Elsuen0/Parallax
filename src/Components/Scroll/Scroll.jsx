4ca972c4ac9a7f20f144e16bd6625fa5e14164ed
unknown <lucas.capart80@gmail.com>
2023-10-20 16:44:17 +0200

    Update Scroll.jsx
    Ajout de la logique permettant de faire un effet parallax avec Rellax et de modifier l'opacité en fonction

--- src/Components/Scroll/Scroll.jsx
+++ src/Components/Scroll/Scroll.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 import styles from "./Scroll.module.scss";
+import Rellax from "rellax";
+import { useRef, useEffect } from "react";
 
 const Scroll = () => {
+  const rellaxRef = useRef(null);
+
+  useEffect(() => {
+    const rellax = new Rellax(rellaxRef.current, {
+      speed: -5,
+    });
+
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const element = document.getElementById("contentScroll");
+      const elementHeight = element.clientHeight;
+
+      // Calculez l'opacité en fonction de la position de défilement
+      const opacity = Math.max(0, (400 - scrollTop) / elementHeight).toFixed(2);
+
+      element.style.opacity = opacity;
+
+      // Mettez à jour l'animation
+      if (opacity === 0) {
+        element.style.animationName = "none"; // Désactivez l'animation lorsque l'opacité est à 0
+      } else {
+        element.style.animationName = "slideIn"; // Réactivez l'animation lorsque l'opacité n'est pas à 0
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      rellax.destroy();
+    };
+  }, []);
   return (
     <div className={styles.scroll}>
-      <div className={styles.contentScroll}>
+      <div className={styles.contentScroll} ref={rellaxRef} id="contentScroll">
         <div className={styles.text}>
           <p>SCROLL</p>
         </div>
