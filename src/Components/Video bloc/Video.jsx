72850f83f249d517d35dab7e9a2818f4b550742e
unknown <lucas.capart80@gmail.com>
2023-10-24 13:40:53 +0200

    Update Video component
    Ajout  du texte d'arrière plan + modifications de fin

--- src/Components/Video bloc/Video.jsx
+++ src/Components/Video bloc/Video.jsx
@@ -11,19 +11,34 @@ const Video = () => {
   return (
     <div className={styles.container}>
       <div className={styles.titleContainer}>
-        <h1 data-aos="fade-up" data-aos-offset="500">
+        <h1 data-aos="fade-up" data-aos-offset="500" data-aos-duration="1000">
           Everest{" "}
         </h1>
-        <h2 data-aos="fade-up">The highest mountain in the world</h2>
+        <h2 data-aos="fade-up" data-aos-offset="400" data-aos-duration="1000">
+          The highest mountain in the world
+        </h2>
       </div>
 
       <div className={styles.background}></div>
-      <img src="./videoBackground.jfif" alt="" data-aos="zoom-in" />
+      <img
+        src="./videoBackground.jfif"
+        alt=""
+        data-aos="zoom-in"
+        data-aos-offset="300"
+        data-aos-duration="1000"
+      />
       <div className={styles.buttonPlay}>
         <img src="./Play.svg" alt="" data-aos="fade-up" data-aos-offset="0" />
       </div>
       <div className={styles.textBackground}>
-        <p>01</p>
+        <p
+          data-aos="fade-right"
+          data-aos-delay="1000"
+          data-aos-duration="1000"
+          data-aos-offset="300"
+        >
+          01
+        </p>
       </div>
     </div>
   );
